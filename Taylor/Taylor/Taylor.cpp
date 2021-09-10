2e47e985c03efc596bcdbb8e5dc8410b75ff48b6
StanislavKhartov <89999222+StanislavKhartov@users.noreply.github.com>
2021-09-10 19:48:58 +0300

    Add input

---
+++ Taylor/Taylor/Taylor.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Input data
+    cout << "Please, enter e\n";
+    long double e;
+    cin >> e;
+    cout << "Please, enter the argument of function sin(x)/x, x∈(-1;1)\n";
+    long double argument;
+    cin >> argument;
+    // Check 
+    if (abs(argument) >= 1)
+    {
+        cout << "Incorrect data\n";
+        return 0;
+    }
+
+}
