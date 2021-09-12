581421983ce03e37305bbac36e3f9cc8729c2aae
StanislavKhartov <89999222+StanislavKhartov@users.noreply.github.com>
2021-09-12 19:41:43 +0300

    entering K

--- Taylor/Taylor/Taylor.cpp
+++ Taylor/Taylor/Taylor.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main()
 {
     // Input data
-    cout << "Please, enter e < 1/10\n";
-    long double e;
-    cin >> e;
+    cout << "Please, enter k\n";
+    int k;
+    cin >> k;
     cout << "Please, enter the argument of function sin(x)/x, x from (-1;1)\n";
     long double argument;
     cin >> argument;
@@ -23,7 +23,7 @@ int main()
     double devisor = 1;
     long double sum = 0;
     long double square = argument * argument;
-    while (fabsl(term) > e)
+    while (fabsl(term) > pow(10, -k))
     {
         sum = sum + term;
         devisor = devisor + 2;
